52efe9870e444cb2c0671ac0ee7ffb3eeee8f0ee
Raja <39787263+crajaramachandran@users.noreply.github.com>
2018-07-17 05:47:00 -0500

    Initial Commit

---
+++ ReservationsProject/Exercise Files/Chapter 2/02_03/02_03_start/reservations/src/main/java/com/frankmoley/landon/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.frankmoley.landon.data.repository;
+
+import com.frankmoley.landon.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room,Long> { //Long corresponds to our long id;
+    Room findByNumber(String number);
+}
